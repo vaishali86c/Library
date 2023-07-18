8954ebd5f898425297911e5d2d9668676216a2c8
vaishali86c <vaishali86c@gmail.com>
2023-07-18 22:50:15 +0530

    Change Style of Form

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addBookButton = document.getElementById("addBookButton");
+    const addBookForm = document.getElementById("addBookForm");
+    const submitBtn = document.querySelector(".submitBtn");
+  
+    // Event listener to show/hide the form
+    addBookButton.addEventListener("click", function () {
+      addBookForm.style.display = "block";
+    });
+  
+    // Event listener for form submission
+    submitBtn.addEventListener("click", function () {
+      addBookForm.style.display = "none";
+    });
+  });
+  
