9ffa739a90b056810c1b53c7fac8f15085f2b4a5
vaishali86c <vaishali86c@gmail.com>
2023-09-22 20:14:31 +0530

    Add login form

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const addBookButton = document.getElementById("addBookButton");
-    const addBookForm = document.getElementById("addBookForm");
-    const submitBtn = document.querySelector(".submitBtn");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const addBookButton = document.getElementById("addBookButton");
+//     const addBookForm = document.getElementById("addBookForm");
+//     const submitBtn = document.querySelector(".submitBtn");
   
-    // Event listener to show/hide the form
-    addBookButton.addEventListener("click", function () {
-      addBookForm.style.display = "block";
-    });
+//     // Event listener to show/hide the form
+//     addBookButton.addEventListener("click", function () {
+//       addBookForm.style.display = "block";
+//     });
   
-    // Event listener for form submission
-    submitBtn.addEventListener("click", function () {
-      addBookForm.style.display = "none";
-    });
-  });
-  
+//     // Event listener for form submission
+//     submitBtn.addEventListener("click", function () {
+//       addBookForm.style.display = "none";
+//     });
+//   });
+  // Get the modal
+  // Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
